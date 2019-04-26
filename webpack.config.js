2304605e6519448d10c5dc6211d24cff9d58716e
Paul Rosset <paul.rosset@canal-plus.com>
2019-04-26 16:20:57 +0200

    Preserve fn name during the minify

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,45 @@
 const path = require('path');
 const CopyWebPackPlugin = require('copy-webpack-plugin');
 const ChromeExtensionReloader = require('webpack-chrome-extension-reloader');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = () => ({
   devtool: 'sourcemap',
   entry: {
     background: './src/background.js',
     content: './src/content.js',
-    options: './src/options.js'
+    options: './src/options.js',
   },
   output: {
     path: path.join(__dirname, 'build'),
-    filename: '[name].js'
+    filename: '[name].js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_fnames: true,
+        },
+      }),
+    ],
   },
   plugins: [
     new CopyWebPackPlugin([
       {
         from: '*',
         context: 'src',
-        ignore: '*.js'
-      }
+        ignore: '*.js',
+      },
     ]),
-    new ChromeExtensionReloader()
-  ]
+    new ChromeExtensionReloader(),
+  ],
 });
