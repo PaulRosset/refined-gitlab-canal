caf3a249036c0bb18259481fa396b7cd4a1af6a5
Paul Rosset <paul.rosset@canal-plus.com>
2019-10-28 21:10:48 +0100

    feat(highlight-thumb): Highlight a thumb in green if you provided a thumb on a MR

--- src/features/highlight-who-thumb.js
+++ src/features/highlight-who-thumb.js
@@ -1,42 +1,49 @@
-import { h } from 'dom-chef';
-import select from 'select-dom';
+import { h } from "dom-chef";
+import select from "select-dom";
 
-import fetch from '../utils/api';
-import { getRepoName } from '../utils/page-detect';
-import { get, set } from '../utils/cache';
+import fetch from "../utils/api";
+import { getRepoName } from "../utils/page-detect";
+import { get, set } from "../utils/cache";
 
 async function populateChild(repoName, displayAvatarForThumbStruct) {
+  const { data: currentUser } = await fetch("get", "/user");
   for (const mR of displayAvatarForThumbStruct) {
     const res = await fetch(
-      'get',
-      `projects/${repoName}/merge_requests/${mR.mrNumber}/award_emoji`
+      "get",
+      `projects/${repoName}/merge_requests/${mR.mrNumber}/award_emoji`,
     );
-    mR.upVotesDiv.style.textAlign = 'center';
+    mR.upVotesDiv.style.textAlign = "center";
     mR.upVotesDiv.appendChild(
-      <div>
+      <div className="highlight-who-thumb">
         {res.data
-          .filter(elem => elem.name === 'thumbsup')
-          .map(elem => (
-            <img
-              width="16"
-              class="avatar avatar-inline s16 js-lazy-loaded has-tooltip"
-              data-original-title={elem.user.name}
-              alt=""
-              src={`${elem.user.avatar_url}?width=16`}
-            />
-          ))}
-      </div>
+          .filter(elem => elem.name === "thumbsup")
+          .map(elem => {
+            if (elem.user.name === currentUser.name) {
+              mR.upVotesDiv["title"] = "You Thumbed UP!";
+              mR.upVotesDiv.children[0].style.color = "#1aaa55";
+            }
+            return (
+              <img
+                width="16"
+                class="avatar avatar-inline s16 js-lazy-loaded has-tooltip"
+                data-original-title={elem.user.name}
+                alt=""
+                src={`${elem.user.avatar_url}?width=16`}
+              />
+            );
+          })}
+      </div>,
     );
   }
 }
 
 async function displayUserWhoThumbMR() {
   const displayAvatarForThumbStruct = select
-    .all('.mr-list .merge-request .controls .issuable-upvotes')
+    .all(".mr-list .merge-request .controls .issuable-upvotes")
     .map(elem => ({
       upVotesDiv: elem,
       mrNumber: /([0-9]+)/.exec(
-        select('.issuable-comments .has-tooltip', elem.parentNode).href
+        select(".issuable-comments .has-tooltip", elem.parentNode).href,
       )[0],
     }));
 
@@ -45,7 +52,7 @@ async function displayUserWhoThumbMR() {
   }
 
   const repoName = getRepoName(
-    select("meta[property='og:title']").getAttribute('content')
+    select("meta[property='og:title']").getAttribute("content"),
   );
 
   const projectID = await get([repoName]);
@@ -53,8 +60,8 @@ async function displayUserWhoThumbMR() {
     populateChild(projectID[repoName], displayAvatarForThumbStruct);
   } else {
     const projects = await fetch(
-      'get',
-      `projects?search=${repoName}&simple=true`
+      "get",
+      `projects?search=${repoName}&simple=true`,
     );
     const projectIDStored = await set({
       [repoName]: projects.data.filter(elem => elem.name === repoName)[0].id,
