5a2fa88d34b2d5390f3cc5846809d36f7ae48f76
prosset <paulrosset96@gmail.com>
2020-10-12 13:08:46 +0200

    Refactor sortWipState function to take Draft state in count

---
+++ src/features/sort_wip_draft_state.js
@@ -0,0 +1,29 @@
+import { h } from "dom-chef";
+import select from "select-dom";
+
+function sortWipDraftState() {
+  const getOriginalRootNodeMR = select(".mr-list");
+  const getAllMrEntry = select.all(".merge-request", getOriginalRootNodeMR);
+  const sortedMR = getAllMrEntry
+    .map((mrEntry) => {
+      const text = select(".merge-request-title-text a", mrEntry).text;
+      return { text, mrEntry };
+    })
+    .sort((a, b) => {
+      const isDraftA = a.text.indexOf("Draft:");
+      const isWipA = a.text.indexOf("WIP:");
+      const isDraftB = b.text.indexOf("Draft:");
+      const isWipB = b.text.indexOf("WIP:");
+
+      return isDraftA + isWipA - (isDraftB + isWipB);
+    })
+    .map((elem) => elem.mrEntry);
+  for (const node in getAllMrEntry) {
+    getAllMrEntry[node].remove();
+  }
+  for (const node in sortedMR) {
+    getOriginalRootNodeMR.appendChild(sortedMR[node]);
+  }
+}
+
+export default sortWipDraftState;
